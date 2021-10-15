6b84be0169f080280409845b4dd9ccf9cdcfacb8
cjpatubo <cdpatubo@up.edu.ph>
2021-10-15 15:59:38 +0800

    1st push (Cams) - added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProductsPageComponent } from './components/products-page/products-page.component';
 import { ProductDetailPageComponent } from './components/product-detail-page/product-detail-page.component';
+import { SearchComponent } from './components/search/search.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: ProductDetailPageComponent },
   { path: 'heroes', component: ProductsPageComponent },
+  { path: 'search', component: SearchComponent}
 ];
 
 @NgModule({
