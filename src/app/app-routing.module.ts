7dae774bfc799555cd35494baad4fcdb4c3c2fee
Karl Steven <lerdrobott@gmail.com>
2021-10-16 12:13:49 +0800

    2ndCommit: Test Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProductsPageComponent } from './components/products-page/products-page.component';
 import { ProductDetailPageComponent } from './components/product-detail-page/product-detail-page.component';
 import { SearchComponent } from './components/search/search.component';
+import { AddToCartPageComponent } from './components/add-to-cart-page/add-to-cart-page.component';
 
 
 const routes: Routes = [
@@ -13,6 +14,7 @@ const routes: Routes = [
   { path: 'products', component: ProductsPageComponent },
   { path: 'heroes', component: ProductsPageComponent },
   { path: 'search', component: SearchComponent },
+  { path: 'cart', component: AddToCartPageComponent}
   
 ];
 
