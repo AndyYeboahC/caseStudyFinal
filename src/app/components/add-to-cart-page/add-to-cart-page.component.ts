94df895edd234b7645e1eb65b5eded4842cb45db
AndyYeboahC <engr.cudjoe@gmail.com>
2021-10-18 21:58:09 +0800

    BACK ON TRACK

--- src/app/components/add-to-cart-page/add-to-cart-page.component.ts
+++ src/app/components/add-to-cart-page/add-to-cart-page.component.ts
@@ -4,31 +4,29 @@ import { ProductService } from 'src/app/services/product.service';
 @Component({
   selector: 'app-add-to-cart-page',
   templateUrl: './add-to-cart-page.component.html',
-  styleUrls: ['./add-to-cart-page.component.scss']
+  styleUrls: ['./add-to-cart-page.component.scss'],
 })
 export class AddToCartPageComponent implements OnInit {
-
-  public products : any = [];
-  public gross  !: number;
-  public grandTotal !: number ;
+  public products: any = [];
+  public gross!: number;
+  public grandTotal!: number;
   public totalItem: number = 0;
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
-    this.productService.getItems().subscribe(res=>{
+    this.productService.getItems().subscribe((res) => {
       this.products = res;
       this.grandTotal = this.productService.getTotalPrice();
-      this.totalItem = res.length;  
+      this.totalItem = res.length;
       this.gross = this.products.quantity * this.products.price;
-      
-    })
+    });
   }
 
-  removeItem(item: any){
+  removeItem(item: any) {
     this.productService.removeCartItem(item);
   }
 
-  emptycart(){
+  emptycart() {
     this.productService.removeAllCart();
   }
 }
--- src/app/components/product-detail-page/product-detail-page.component.ts
+++ src/app/components/product-detail-page/product-detail-page.component.ts
@@ -15,7 +15,7 @@ export class ProductDetailPageComponent implements OnInit {
   product: Product | undefined;
 
   public productList: any;
-  public gross !: number ;
+
 
   constructor(
     private route: ActivatedRoute,
@@ -29,9 +29,8 @@ export class ProductDetailPageComponent implements OnInit {
     this.productService.getItems().subscribe((res) => {
       this.productList = res;
 
-      this.productList.forEach((a: any) => {
-        Object.assign(a, {total: a.price});
-        
+      this.productList.forEach((a: any,) => {
+        Object.assign(a, {total: a.price });
       });
 
 
@@ -53,8 +52,8 @@ export class ProductDetailPageComponent implements OnInit {
   //CHECK IF EXISTING SI ITEM NA UNG ITEM USING cartItem.id
     if(this.productList.some((cartItem:any,) => cartItem.id === product.id)){
       product.quantity++;
-      this.gross = product.quantity * product.price;
-      
+
+
   // first attempt, quantity will be = to 1 because of line 32 to 33... what we need is grab that quantity then increment by 1
     } else {
   // KUNG WALA, ILALAGAY NYA UNG ITEM NA UN SA ARRAY.. CALLING addtoCart
